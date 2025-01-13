339a8a516545ba9b35577b23385f3e9e4a923f6b
Ramganga <ram886694@gmail.com>
2025-01-14 02:57:04 +0530

    Print All Subset

--- recursion/Printsubset.cpp
+++ recursion/Printsubset.cpp
@@ -1,23 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
-void printSubset(vector<int> &arr, int idx, vector<int> &ans){
+void printSubset(vector<int> &arr, int idx, vector<int> &ans, vector<vector<int>> &result){
     if(idx >= arr.size()){
-        for(int num:ans){
-            cout<< num<<" ";
-        }cout<<endl;
+        // for(int num:ans){
+        //     cout<< num<<" ";
+        // }cout<<endl;
+        result.push_back(ans);
         return;
     }
     //include
     ans.push_back(arr[idx]);
-    printSubset(arr, idx+1, ans);
+    printSubset(arr, idx+1, ans, result);
     ans.pop_back(); //back tacking
     //exclude
-    printSubset(arr, idx+1, ans);
+    printSubset(arr, idx+1, ans, result);
 }
 
 int main(){
     vector<int>arr={1,2,3};
     vector<int>ans;
-    printSubset(arr, 0,ans);
+    vector<vector<int>>result;
+    printSubset(arr, 0,ans, result);
+    for(auto x:result){
+        for(auto y:x){
+            cout<<y<<" ";
+        }
+    }
     return 0;
 }
