a309b3606d1019163e731cba8813be56567fb84b
Ramganga <ram886694@gmail.com>
2025-01-19 12:12:18 +0530

    Constructor

---
+++ OOPs/constructor.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Example{
+    public:
+    // Default constructor
+    Example(){
+        cout << "Default constructor called" << endl;
+    }
+    // Parameterized constructor
+    Example(int x){
+        cout << "Parameterized constructor called with value: " << x << endl;
+    }
+    // Copy constructor
+    Example(const Example &obj){
+        cout << "Copy constructor called" << endl;
+    }
+};
+int main(){
+    Example obj1; // Default constructor called
+    Example obj2(10); // Parameterized constructor called with value: 10
+    Example obj3 = obj1; // Copy constructor called
+    
+    return 0;
+}
