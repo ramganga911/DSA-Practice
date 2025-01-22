e32307662772785d6dbee4d169bbccc9f447ba7d
Ramganga <ram886694@gmail.com>
2025-01-22 18:37:55 +0530

    Update

--- HashTable/countsubArrayequal01.cpp
+++ HashTable/countsubArrayequal01.cpp
@@ -22,10 +22,48 @@ long long int countSubarrWithEqualZeroAndOne(vector<int>&arr, int n)
     }
     return count;
 }
+// Function to find the length of largest subarray with 0 sum
+int maxLen(vector<int>& arr) {
+        // code here
+        int ans =0;
+        int sum =0;
+        unordered_map<int, int>mp;
+        int n = arr.size();
+        mp[0]=-1;
+        for(int i=0; i<n; i++){
+            sum+=arr[i];
+            if(mp.find(sum)!=mp.end()){
+                ans = max(ans,i-mp[sum]);
+            }else{
+                mp[sum]=i;
+            }
+        }
+        return ans;
+    }
+    // Function to count subarray with 0 sum
+    int findSubarray(vector<int> &arr)
+    {
+        int n = arr.size();
+        int count = 0;
+        int sum = 0;
+        unordered_map<int, int> mp;
+        mp[0] = 1;
+        for (int i = 0; i < n; i++)
+        {
+            sum += arr[i];
+            if (mp.find(sum) != mp.end())
+            {
+                count += mp[sum];
+            }
+            mp[sum]++;
+        }
+        return count;
+    }
 int main(){
     vector<int> arr = {1, 0, 0, 1, 0, 1, 1};
     int n = arr.size();
     long long ans = countSubarrWithEqualZeroAndOne(arr, n);
     cout << ans << endl;
+    
     return 0;
 }
