90a72d1bbf052d6de573c3f551afa63073915cad
Ramganga <ram886694@gmail.com>
2025-01-26 18:52:48 +0530

    Update in Longest Substring Without repeating character

--- Sliding_window/longest_substring_without_repiting.cpp
+++ Sliding_window/longest_substring_without_repiting.cpp
@@ -1,31 +1,58 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-string lonestSubstring(string s){
-    unordered_map<char,int> seen;
-    int len=0;
-    int begin =0, end=0;
-    string ans ="";
+// string longestSubstring(string s){
+//     unordered_map<char,int> seen;
+//     int len=0;
+//     int begin =0, end=0;
+//     string ans ="";
 
-    while(end <s.size()){
+//     while(end <s.size()){
+//         char current = s[end];
+//         if(seen.count(current)==1 && seen[current] >=begin){
+//             begin = seen[current]+1;
+//         }else{
+//             seen[current] = end;
+//             end++;
+//         }
+
+//         if(end- begin >len){
+//             len = end- begin;
+//             ans = s.substr(begin, end-begin);
+//         }
+//     }
+//     return ans;
+// }
+
+int longestSubstring(string &s)
+{
+    int n = s.size();
+    int start = 0, end = 0, len = 0;
+    unordered_set<char> st;
+
+    while (end < n)
+    {
         char current = s[end];
-        if(seen.count(current)==1 && seen[current] >=begin){
-            begin = seen[current]+1;
-        }else{
-            seen[current] = end;
-            end++;
+
+        // If current character is already in the set, remove from the start
+        while (st.find(current) != st.end())
+        {
+            st.erase(s[start]);
+            start++;
         }
 
-        if(end- begin >len){
-            len = end- begin;
-            ans = s.substr(begin, end-begin);
-        }
+        // Add current character to the set and move the end pointer
+        st.insert(current);
+        end++;
+
+        // Update the maximum length
+        len = max(len, end - start);
     }
-    return ans;
+    return len;
 }
-int main(){
+int main()
+{
     string str = "abcabcbb";
-    string ans = lonestSubstring(str);
+    int ans = longestSubstring(str);
     cout << ans;
     return 0;
 }
-
