1fc9295ac7369417380aaea0df2e40d6725548dc
Ramganga <ram886694@gmail.com>
2025-02-01 20:14:11 +0530

    Height of Tree And Number of Node

---
+++ Linked_List/height.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        this->data = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+int height(Node *root)
+{
+    if (root == NULL)
+        return 0;
+    int left = height(root->left);
+    int right = height(root->right);
+    return max(left, right) + 1;
+}
+int countNode(Node* root){
+    if(root == NULL) return 0;
+    return 1+ countNode(root->left)+ countNode(root->right);
+}
+int main()
+{
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    root->right->right->right = new Node(8);
+    root->right->right->right->right = new Node(9);
+    cout << "Height of the tree is: " << height(root);
+    cout << "\n Number of nodes in the tree is: " << countNode(root);
+    return 0;
+}
